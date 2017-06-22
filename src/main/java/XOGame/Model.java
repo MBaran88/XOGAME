e33725637b115cc370a408a6928960664ef0bd27
MBaran88 <zdzibq@gmail.com>
2017-06-22 20:23:41 +0200

    added few checkers

--- src/main/java/XOGame/Model.java
+++ src/main/java/XOGame/Model.java
@@ -1,57 +1,66 @@
 package XOGame;
 
 
-import static XOGame.Values.O;
-
 /**
  * Created by RENT on 2017-06-22.
  */
 public class Model {
-    int[][] board = new int[3][3];
-    public int movesCounter = 0;
+    public static final char DefaultCharInit = '\u0000';
 
-    public void setChoice(Integer field, Values value) {
-        Integer matrixValue = null;
-        if (value == Values.X){
-            matrixValue = -1;
-        }
-        else if (value == Values.O){
-            matrixValue = 1;
-        }
-        board[field % 3][field / 3] = matrixValue;
-        movesCounter++;
+    /*[2,0] [2,1] [2,2]
+      [1,0] [1,1] [1,2]
+      [0,0] [0,1] [0,2]
+     */
+    public boolean checkBoard(char board[][], char symbol) {
+        if (checkDiagonals(board, symbol) || checkCols(board, symbol) || checkRows(board, symbol))
+            return true;
+        return false;
     }
 
-    public boolean checkBoard(){
-        int sumDiagonalLR = 0;
-        int sumDiagonalRL = 0;
-        int sumColumns = 0;
-        int sumVerses = 0;
+    public boolean checkDraw(char board[][]) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (board[i][j] == DefaultCharInit)
+                    return false;
+            }
+        }
+        return true;
+    }
 
-        for (int i = 0; i <=2 ; i++) {
-            sumDiagonalLR += board[i][i];
+    public boolean checkDiagonals(char board[][], char symbol) {
+        int TLDiagonal = 0;
+        int BLDiagonal = 0;
+
+        for (int i = 0; i <2 ; i++) {
+            if (board[i][i]==symbol)
+                TLDiagonal++;
         }
-        for (int i = 0; i <=2 ; i++) {
-            sumDiagonalRL += board[i][i -2];
-        }
-        if (Math.abs(sumDiagonalLR) == 3 || Math.abs(sumDiagonalRL) == 3){
+        if (TLDiagonal==3){
             return true;
         }
-        for (int i = 0; i <=2 ; i++) {
-            for (int j = 0; j <=2 ; j++) {
-                sumColumns += board[j][i];
-                sumVerses += board[i][j];
+        for (int i = 0; i <2 ; i++) {
+            if (board[i][2-i]==symbol)
+                BLDiagonal++;
             }
-            if (Math.abs(sumColumns) == 3 || Math.abs(sumVerses) == 3) {
+        if (BLDiagonal == 3){
                 return true;
+        }
+        return false;
+        }
+    public boolean checkCols(char board[][],char symbol){
+        int sumCols = 0;
+        for (int i = 0; i <2 ; i++) {
+            for (int j = 0; j <2; j++) {
+                if (symbol==board[i][j])
+                    sumCols++;
+                if (sumCols==3){
+                    return true;
+                }
             }
-            else {
-                sumColumns = 0;
-                sumVerses = 0;
-            }
+        sumCols=0;
         }
         return false;
     }
 
-
 }
+
