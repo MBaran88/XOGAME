d98576a15e33b1d446f9ce5cc6d6914042005976
MBaran88 <zdzibq@gmail.com>
2017-06-26 17:34:47 +0200

    added View class

--- src/main/java/XOGame/View.java
+++ src/main/java/XOGame/View.java
@@ -1,5 +1,6 @@
 package XOGame;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 /**
@@ -15,6 +16,42 @@ public class View {
         frame = new JFrame("X,O Game (Tic Tac Toe)");
         buttons = new JButton[3][3];
         ButtonPanel = new JPanel();
-        
+        Label = new JLabel("JLabelString");
+        for (int i = 0; i <3 ; i++) {
+            for (int j = 0; j <3 ; j++) {
+                buttons[j][i]= new JButton("");
+                ButtonPanel.add(buttons[j][i]);
+            }
+        }
+        ButtonPanel.setLayout(new GridLayout(3,3));
+        frame.add(ButtonPanel,BorderLayout.CENTER);
+        frame.add(Label,BorderLayout.SOUTH);
+        frame.setVisible(true);
+        frame.setSize(500,350);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void chooseLocation(int X_Coordinate, int Y_Coordinate, boolean isX_Turn){
+        if (isX_Turn){
+            buttons[X_Coordinate][Y_Coordinate].setText("X");
+        } else {
+          buttons[X_Coordinate][Y_Coordinate].setText("O");
+        }
+    }
+    public void gameOver(){
+        for (int i = 0; i <3 ; i++) {
+            for (int j = 0; j <3 ; j++) {
+                buttons[j][i].setEnabled(false);
+            }
+        }
+    Label.setText("Game Over!");
+    }
+    public void Draw(){
+        for (int i = 0; i <3 ; i++) {
+            for (int j = 0; j <3 ; j++) {
+                buttons[j][i].setEnabled(false);
+            }
+        }
+    Label.setText("Draw!");
     }
 }
